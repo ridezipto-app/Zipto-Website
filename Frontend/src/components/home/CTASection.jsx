dc46e3780941f59a75d35c9e1dab55edb1b6fd29
ridezipto-app <ride.zipto@gmail.com>
2026-04-12 00:39:46 +0530

    Update CTASection.jsx

--- Frontend/src/components/home/CTASection.jsx
+++ Frontend/src/components/home/CTASection.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function LaunchingSoon() {
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center px-6">
+   <div className="min-h-screen bg-gradient-to-br from-blue-600 to-orange-500 text-white flex flex-col items-center justify-center px-6">
 
       {/* Heading */}
       <h1 className="text-3xl md:text-5xl font-semibold text-center mb-10">
-        Launching Soon in <br />
+        Trusted by Thousands, <br />
         <span className="flex items-center justify-center gap-2">
-          Bhubaneswar 🚀
+           Growing Every Day 🚀
         </span>
       </h1>
 
       {/* Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
+     <div className="border border-white/20 backdrop-blur-md bg-white/10 rounded-xl p-6 flex items-center gap-4">
 
         {/* Card 1 */}
         <div className="border border-gray-800 rounded-xl p-6 flex items-center gap-4 bg-[#0c0c0c]">
@@ -53,7 +53,7 @@ export default function LaunchingSoon() {
         <p className="text-gray-400 mb-2 tracking-widest">~~~ Our Mission ~~~</p>
 
         <p className="text-lg md:text-xl text-gray-200 max-w-xl">
-          Launching soon. Join us in redefining local delivery.
+          Building Odisha’s fastest hyperlocal delivery network
         </p>
       </div>
     </div>
