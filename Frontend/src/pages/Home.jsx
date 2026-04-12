73ba3a63b488c8347d20de7589ab4df502d06756
ridezipto-app <ride.zipto@gmail.com>
2026-04-12 23:14:51 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,22 +5,17 @@ import ForBusinessSection from "../components/home/ForBusinessSection";
 import CTASection from "../components/home/CTASection";
 import Footer from "../components/layout/Footer";
 import BecomeRider from "../components/home/BecomeRider";
-
 export default function Home() {
   return (
-    <div className="w-full overflow-x-hidden">
+    <>
       <Navbar />
-      
-      {/* Main Content Wrapper */}
-      <div className="max-w-[1400px] mx-auto px-4 sm:px-6 md:px-10 lg:px-16">
-        <HeroSection />
-        <ServicesSection />
-        <ForBusinessSection />
-        <BecomeRider />
-        <CTASection />
-      </div>
-
+      <HeroSection />
+      <ServicesSection />
+      <ForBusinessSection />
+      <BecomeRider />
+      <CTASection />
       <Footer />
-    </div>
+      
+    </>
   );
 }
